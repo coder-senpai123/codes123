707037b06f886845f8c38bc1378223bb4468025c
Robbie <yashoct568@gmail.com>
2024-05-11 15:46:38 +0530

    final

--- second.cpp
+++ second.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main(){
     int a ;
-    cin
-    
+    cin>>a ;
+    for(int i = 0 ; i < a ; i++){
+        cout<<a;
+        for(int j = i ; j > 0 ; j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
